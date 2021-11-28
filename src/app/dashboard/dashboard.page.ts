94961f5d4950f8cc8528e8d39b3f75342f1f3eed
Skoms <andrew.m.s.97@hotmail.com>
2021-11-28 12:51:51 +0100

    feat(darkMode): implement darkmode, fix cards

--- src/app/dashboard/dashboard.page.ts
+++ src/app/dashboard/dashboard.page.ts
@@ -1,7 +1,9 @@
+import { Element } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { MenuController } from '@ionic/angular';
 import { AuthService } from '../services/auth.service';
+import { StorageService } from '../services/storage.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,13 +11,41 @@ import { AuthService } from '../services/auth.service';
   styleUrls: ['./dashboard.page.scss'],
 })
 export class DashboardPage implements OnInit {
+  prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+  darkMode: boolean;
   constructor(
     private auth: AuthService,
     private menu: MenuController,
-    private router: Router
+    private router: Router,
+    private storage: StorageService
   ) {}
 
-  ngOnInit() {}
+  async ngOnInit() {
+    await this.ensureLogin();
+    await this.checkForDarkMode();
+    this.prefersDark.addEventListener('change', (e) =>
+      this.toggleDarkMode(e.matches)
+    );
+  }
+
+  public async checkForDarkMode() {
+    const darkMode = await this.storage.get('darkMode');
+    if (darkMode) {
+      this.toggleDarkMode(darkMode);
+      return;
+    }
+    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+    if (prefersDark.matches) {
+      this.toggleDarkMode(prefersDark.matches);
+      return;
+    }
+  }
+
+  public async toggleDarkMode(isActive: boolean = this.darkMode) {
+    this.darkMode = isActive;
+    document.body.classList.toggle('dark', isActive);
+    await this.storage.set('darkMode', isActive);
+  }
 
   public goTo(path: string, isMenu: boolean = true) {
     this.router.navigateByUrl(path);
